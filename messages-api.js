4cde9f730b8ef0f47ca1c4cf1ed9ab2aaceec5d1
hakan0814 <h.k.cuhadar@gmail.com>
2019-11-09 18:37:31 +0100

    Express API completed

--- messages-api.js
+++ messages-api.js
@@ -2,40 +2,44 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
 
 app.get("/", (request, response) => {
-  console.log(`Get response from messages-api.js`);
-  response.send(`Get response from messages-api.js`);
+  response.status(200).send(`Messages-api.js connected!`);
 });
-
-app.post("/messages", (request, response) => {
-  // When a request is sent to the endpoint, it should log the `text` property of the body to the console, and it should respond with a JSON object:
-  console.log(request.body);
-  if (request.body) {
-    return response.status(200).json({
-      message: "Message received loud and clear"
-    });
-  } else if (!request.body) {
-    return response.status(400).send({
-      message: "Message not received"
-    });
-  } else {
-    return response.status(500).send({
-      message: "Something went wrong"
-    });
-  }
-});
-
-// Perform the following validation: if the body does NOT have a `text` property or the string is empty, then send a "Bad Request" HTTP status code to the client.
 
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`Messages API listening on port :${port}`);
+});
+
+let pingCount = 0;
+
+app.post("/messages", (request, response, next) => {
+  pingCount++;
+  if (request.body.text && pingCount <= 5) {
+    const text = "Message received loud and clear";
+    return response.status(200).json({
+      message: text,
+      posted: request.body.text
+    });
+  } else if (!request.body.text) {
+    const text = "Bad Request";
+    return response.status(400).json({
+      message: text,
+      posted: request.body.text
+    });
+  } else if (request.body.text && pingCount > 5) {
+    const text = "Too Many Requests";
+    return response.status(429).json({
+      message: text,
+      posted: request.body.text
+    });
+  } else {
+    return response.status(500).json({
+      message: "Something went wrong"
+    });
+  }
 });
